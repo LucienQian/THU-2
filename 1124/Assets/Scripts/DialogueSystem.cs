0553c9c996ddc61a8ba0de0da727cb626f7794b2
LucienQian <jean20011116@gmail.com>
2022-12-01 15:55:24 +0800

    1201

---
+++ 1124/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+
+namespace LUCIEN
+{
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public class DialogueSystem : MonoBehaviour
+    {
+        #region 資料區域
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData DialogueOpening;
+
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContent;
+        private GameObject gotriangle; 
+        #endregion
+
+        #region 事件
+        private void Awake()
+        {
+
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            gotriangle = GameObject.Find("對話完成圖示");
+            gotriangle.SetActive(false);
+        } 
+        #endregion
+    }
+}
